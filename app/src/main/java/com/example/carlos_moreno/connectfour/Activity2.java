73b25ae2e9624deb15150f3575c14e4130ffa756
Carlos Moreno Vargas <carlos-moreno@bts.k12.nj.us>
2019-03-04 10:06:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/carlos_moreno/connectfour/Activity2.java
@@ -0,0 +1,13 @@
+package com.example.carlos_moreno.connectfour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
